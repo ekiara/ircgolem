97f344fd5fad824cad92bb761f6dc823460602c7
Eric Kiara <ekiara@gmail.com>
2016-04-06 15:34:13 +0300

    Initial commit

---
+++ ircgolem/ircgolem.py
@@ -0,0 +1,10 @@
+# Script Name   : ircgolem.py
+# Author        : Eric Kiara
+# Created       : 06 April 2016
+# Last Modified : 06 April 2016
+# Version       : 0.1
+
+# Modifications : N/A
+
+# Description   : IRC Bot for Freenode Channels
+
